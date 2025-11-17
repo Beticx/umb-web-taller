abc6286e5126b6739a03c20749e81c8ffc4dae6c
Beticx <147877867+Beticx@users.noreply.github.com>
2025-11-17 23:29:41 +0000

    Fix: db.php lee variables del entorno

--- api/db.php
+++ api/db.php
@@ -1,9 +1,11 @@
 <?php
-// Configuración para usar las Variables de Entorno de Render
-$DB_HOST = getenv('DB_HOST');
-$DB_USER = getenv('DB_USER');
-$DB_PASS = getenv('DB_PASS');
-$DB_NAME = getenv('DB_NAME');
+// Configuración para usar las Variables de Entorno de Railway
+// NOTA: Los nombres de las variables (KEYS) deben coincidir EXACTAMENTE
+// con las que Railway genera y las que pusiste en Render.
+$DB_HOST = getenv('MYSQLHOST'); 
+$DB_USER = getenv('MYSQLUSER'); 
+$DB_PASS = getenv('MYSQLPASSWORD'); 
+$DB_NAME = getenv('MYSQLDATABASE'); 
 
 // 1. Conexión usando las variables de entorno
 $conexion = mysqli_connect(
@@ -11,15 +13,14 @@ $conexion = mysqli_connect(
     $DB_USER, 
     $DB_PASS, 
     $DB_NAME,
-    3306 
+    3306 // Usamos el puerto 3306
 );
 
 // 2. Validación de conexión
 if (mysqli_connect_errno()) {
-    // Es buena práctica no exponer errores internos en producción
-     //error_log("Error al conectar a MySQL: " . mysqli_connect_error());
+    // Si la conexión falla (por variables incorrectas o la DB apagada)
      http_response_code(500); // Internal Server Error
-     echo json_encode(['error' => 'Error de conexión a la base de datos']);
+     echo json_encode(['error' => 'Error de conexión a la base de datos.']);
      exit();
 }
 ?>
